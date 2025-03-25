f763b257786d11637a6f6be5e5b060f8f8cd7b70
heblacky <heblacky@gmail.com>
2025-03-26 00:16:01 +0200

    Add favicon and update document head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,10 @@ import { fontSans } from "@/config/fonts";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
